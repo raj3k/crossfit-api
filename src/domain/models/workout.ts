e7d0fca97339cd8f0a43075a9702eede0ff4d7c0
raj3k <rajkosky@gmail.com>
2022-11-10 21:44:48 +0100

    interface for workout model

--- src/domain/models/workout.ts
+++ src/domain/models/workout.ts
@@ -1,7 +1,18 @@
 export interface WorkoutRequestModel {
-
+    name: string;
+    mode: string;
+    equipment: string[];
+    exercises: string[];
+    trainerTips: string[];
 }
 
 export interface WorkoutResponseModel {
-    
+    id: string;
+    name: string;
+    mode: string;
+    equipment: string[];
+    exercises: string[];
+    trainerTips: string[];
+    createdAt: string;
+    updatedAt: string;
 }
