f5fff9748a56b2c4df2ef7db36f5f3bc5380a4ec
raj3k <rajkosky@gmail.com>
2022-11-17 00:21:14 +0100

    implemented WorkoutModel using Mongoose model

--- src/domain/models/workout.ts
+++ src/domain/models/workout.ts
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+
 export interface WorkoutRequestModel {
     name: string;
     mode: string;
@@ -7,7 +9,7 @@ export interface WorkoutRequestModel {
 }
 
 export interface WorkoutResponseModel {
-    id: string;
+    _id: string;
     name: string;
     mode: string;
     equipment: string[];
@@ -16,3 +18,19 @@ export interface WorkoutResponseModel {
     createdAt: string;
     updatedAt: string;
 }
+
+const workoutSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true, unique: true },
+        mode: { type: String, required: true },
+        equipment: { type: [String], required: true },
+        exercises: { type: [String], required: true },
+        trainerTips: { type: [String] }      
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    },
+);
+
+export const WorkoutModel = mongoose.model<WorkoutResponseModel>('Workout', workoutSchema);
