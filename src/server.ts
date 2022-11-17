8d65989f0225447c96df50125bdbb029a6356fdb
raj3k <rajkosky@gmail.com>
2022-11-17 21:59:53 +0100

    cors & helmet added to project

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const server = express();
 
 server.use(express.json());
 server.use(morgan('dev'));
+server.use(cors());
+server.use(helmet());
 
 export default server;
