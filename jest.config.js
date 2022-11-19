5a59003eff391e14a298aeb39e79edd4adcb4398
raj3k <rajkosky@gmail.com>
2022-11-19 10:03:53 +0100

    added module-alias-jest to resolve issues with aliases in tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+const aliases = require('module-alias-jest/register');
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: aliases.jest
 };
