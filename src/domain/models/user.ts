bd9b3a359804dd02f132057be63b270e76f11f61
raj3k <rajkosky@gmail.com>
2022-11-20 21:17:51 +0100

    implemented hashing password in pre save and method to check password

--- src/domain/models/user.ts
+++ src/domain/models/user.ts
@@ -1,12 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose, { Document } from 'mongoose';
+import bcrypt from 'bcrypt';
 
-export interface User {
+export interface User extends Document {
     _id: string,
     firstName: string,
     lastName: string,
     dateOfBirth: string,
     email: string,
     password: string
+
+    isValidPassword(password: string): Promise<Error | boolean>
 }
 
 const userSchema = new mongoose.Schema(
@@ -22,5 +25,23 @@ const userSchema = new mongoose.Schema(
         versionKey: false
     }
 )
+
+userSchema.pre<User>('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next()
+    }
+
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+
+    next();
+});
+
+userSchema.methods.isValidPassword = async function (
+    password: string
+): Promise<Error | boolean> {
+    return await bcrypt.compare(password, this.password);
+};
 
 export const UserModel = mongoose.model<User>('User', userSchema);
