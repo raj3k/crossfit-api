6d493a3571d215d119c51c46142dce3a64034cc7
raj3k <rajkosky@gmail.com>
2022-11-20 15:08:37 +0100

    basic user schema implemented

---
+++ src/domain/models/user.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+export interface User {
+    _id: string,
+    firstName: string,
+    lastName: string,
+    dateOfBirth: string,
+    email: string,
+    hashedPassword: string
+}
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        dateOfBirth: { type: String },
+        email: { type: String, required: true, index: true },
+        hashedPassowrd: { type: String, required: true }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+)
+
+export const UserModel = mongoose.model<User>('User', userSchema);
