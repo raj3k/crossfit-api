258e0b1412b2b950cf92c0886dce00f452ba4017
raj3k <rajkosky@gmail.com>
2022-11-22 22:23:57 +0100

    refactored wrapper

--- src/data/interfaces/data-sources/mongoose-model-wrapper.ts
+++ src/data/interfaces/data-sources/mongoose-model-wrapper.ts
@@ -1,7 +1,8 @@
-export interface MongooseWrokoutModelWrapper {
+export interface MongooseModelWrapper {
     find(): any
     findById(id: string): any
-    create(workout: any): any
+    findOne(conditions: Object, projection?: any, options?: Object, callback?: Function): any
+    create(data: any): any
     findByIdAndUpdate(id: string, data: any, options?: any, callback?: any): any
     findByIdAndDelete(id: string): void
 }
